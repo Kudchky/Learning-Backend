0d53ab763bf07ac402d3e58b85f77470497e3c55
Pol Castillo <rpcastilloy@gmail.com>
2024-04-23 17:47:36 -0500

    done exercise 3

--- Exceptions/Main.java
+++ Exceptions/Main.java
@@ -1,11 +1,21 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         //Exception01.iniciarApp(sc);
-        Exception02.convertirEntero(sc);
-        sc.close();
+        //Exception02.convertirEntero(sc);
+        try {
+            Exception03.printPositionArray(sc);
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println(e.getMessage());
+        } catch(InputMismatchException e) {
+            System.out.println("Ingresa un valor entero");
+        } finally {
+            sc.close();
+            System.out.println("Terminado");
+        }
     }
 
 
