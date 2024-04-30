9a9e1b212a6e53e1a6db9726a950f980abd2000b
Pol Castillo <rpcastilloy@gmail.com>
2024-04-30 06:53:28 -0500

    Creando la clase Orden

---
+++ 04-AppVentas/Orden.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class Orden {
+    private int idOrden;
+    private ArrayList<Producto> listaProductos;
+    public static int contadorOrdenes;
+    public static final int MAX_PRODUCTOS = 5;
+
+    public Orden() {
+        this.idOrden = ++contadorOrdenes;
+        this.listaProductos = new ArrayList<>();
+    }
+
+    public int getIdOrden() {
+        return idOrden;
+    }
+
+    public ArrayList<Producto> getListaProductos() {
+        return listaProductos;
+    }
+
+    public void agregarProducto(Producto producto) {
+        if (this.listaProductos.size() < MAX_PRODUCTOS) {
+            listaProductos.add(producto);
+            System.out.println("Producto se agrego correctamente");
+        } else {
+            System.out.println("No se puede agregar producto, 5 productos por Orden");
+        }
+    }
+
+    public double calcularTotal() {
+        double total = 0;
+        for (Producto el : this.listaProductos) {
+            total += el.getPrecio();
+        }
+
+        return total;
+    }
+
+    public void mostrarOrden() {
+        System.out.println("Orden{" +
+                "idOrden=" + idOrden +
+                ", listaProductos=" + listaProductos +
+                '}');
+    }
+
+}
