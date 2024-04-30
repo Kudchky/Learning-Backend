15d169d52bac820af9d0ff674e9d677b23d87826
Pol Castillo <rpcastilloy@gmail.com>
2024-04-30 06:32:13 -0500

    Creando la clase Producto

---
+++ 04-AppVentas/Producto.java
@@ -0,0 +1,44 @@
+public class Producto {
+    private int idProducto;
+    private String nombre;
+    private double precio;
+    public static int contadorProductos;
+
+    public Producto() {
+    }
+
+    public Producto(String nombre, double precio) {
+        this.idProducto = ++contadorProductos;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "idProducto=" + idProducto +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+}
