def4b4cb20e717692f5eb440c34190fb255c1572
Pol Castillo <rpcastilloy@gmail.com>
2024-04-30 12:42:37 -0500

    codeando y terminando clases

---
+++ 05-AppMundoPC/com/gm/mundopc/Computadora.java
@@ -0,0 +1,4 @@
+package com.gm.mundopc;
+
+public class Computadora {
+}
---
+++ 05-AppMundoPC/com/gm/mundopc/Raton.java
@@ -0,0 +1,24 @@
+package com.gm.mundopc;
+
+public class Raton extends DispositivoEntrada{
+    private int idRaton;
+    private static int contadorRatones;
+
+    public Raton(String tipoEntrada, String marca) {
+        super(tipoEntrada, marca);
+        this.idRaton = ++Raton.contadorRatones;
+    }
+
+    public int getIdRaton() {
+        return idRaton;
+    }
+
+    @Override
+    public String toString() {
+        return "Raton{" +
+                "idRaton=" + idRaton +
+                ", tipoEntrada='" + tipoEntrada + '\'' +
+                ", marca='" + marca + '\'' +
+                '}';
+    }
+}
