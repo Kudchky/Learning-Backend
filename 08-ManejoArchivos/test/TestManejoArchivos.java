9316f60a91afd127f0f7d2b4caceee58fd84a4da
Pol Castillo <rpcastilloy@gmail.com>
2024-05-08 23:01:46 -0500

    creando el método anexarArchivo para append información al archivo y no se sobrescriba

--- 08-ManejoArchivos/test/TestManejoArchivos.java
+++ 08-ManejoArchivos/test/TestManejoArchivos.java
@@ -4,10 +4,10 @@ import static manejarchivos.ManejoArchivos.*;
 
 public class TestManejoArchivos {
     public static void main(String[] args) {
-        var rutaArchivo = "pruba02.txt";
+        var rutaArchivo = "prueba01.txt";
         //crearArchivo(nombreArchivo);
         anexarArchivo(rutaArchivo, "Hola desde aca en Java");
-        //Aca se esta sobrescribiendo en el archivo, queremos que hay continuidad
+        //Aca se está sobrescribiendo en el archivo, queremos que hay continuidad
         anexarArchivo(rutaArchivo,"Hola doc");
     }
 }
