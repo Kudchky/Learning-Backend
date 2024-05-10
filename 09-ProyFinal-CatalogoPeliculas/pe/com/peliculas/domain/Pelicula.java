2c466cd1aa7eaffb05658c52fbce09c4f38b0e3d
Pol Castillo <rpcastilloy@gmail.com>
2024-05-10 18:19:19 -0500

    actualizando clase pelicula

--- 09-ProyFinal-CatalogoPeliculas/pe/com/peliculas/domain/Pelicula.java
+++ 09-ProyFinal-CatalogoPeliculas/pe/com/peliculas/domain/Pelicula.java
@@ -1,20 +1,13 @@
 package pe.com.peliculas.domain;
 
 public class Pelicula {
-    private int idNombre;
     private String nombre;
-    public static int countId = 0;
 
     public Pelicula() {
-        this.idNombre = ++Pelicula.countId;
     }
 
     public Pelicula(String nombre) {
         this.nombre = nombre;
-    }
-
-    public int getIdNombre() {
-        return this.idNombre;
     }
 
     public String getNombre() {
@@ -28,8 +21,7 @@ public class Pelicula {
     @Override
     public String toString() {
         return "Pelicula{" +
-                "idNombre=" + idNombre +
-                ", nombre='" + nombre + '\'' +
+                "nombre='" + nombre + '\'' +
                 '}';
     }
 }
