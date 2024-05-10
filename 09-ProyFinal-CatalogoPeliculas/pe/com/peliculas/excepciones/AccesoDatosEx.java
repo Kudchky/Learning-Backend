5bd26d0ae3e3dc94f3975714437510548f1856b7
Pol Castillo <rpcastilloy@gmail.com>
2024-05-10 15:12:07 -0500

    Terminando la Clase AccesoDatosEx para las excepciones

--- 09-ProyFinal-CatalogoPeliculas/pe/com/peliculas/excepciones/AccesoDatosEx.java
+++ 09-ProyFinal-CatalogoPeliculas/pe/com/peliculas/excepciones/AccesoDatosEx.java
@@ -1,4 +1,19 @@
 package pe.com.peliculas.excepciones;
 
-public class AccesoDatosEx {
+public class AccesoDatosEx extends Exception {
+    public AccesoDatosEx(String message) {
+        super(message);
+    }
+}
+
+class EscrituraDatosEx extends AccesoDatosEx {
+    public EscrituraDatosEx() {
+        super("Error de Escritura en el archivo...!");
+    }
+}
+
+class LecturaDatosEx extends AccesoDatosEx {
+    public LecturaDatosEx() {
+        super("Error de Lectura al acceder al archivo....!");
+    }
 }
