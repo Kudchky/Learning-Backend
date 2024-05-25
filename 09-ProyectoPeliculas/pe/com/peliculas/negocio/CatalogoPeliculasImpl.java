e4f33c31307a9fc6456ac0bd2c6c0d8d9cecd5a3
Pol Castillo <rpcastilloy@gmail.com>
2024-05-25 09:42:30 -0500

    Terminando de codear los metodos del paquete de negocio, CatalogoPeliculas

--- 09-ProyectoPeliculas/pe/com/peliculas/negocio/CatalogoPeliculasImpl.java
+++ 09-ProyectoPeliculas/pe/com/peliculas/negocio/CatalogoPeliculasImpl.java
@@ -1,4 +1,49 @@
 package pe.com.peliculas.negocio;
 
-public class CatalogoPeliculasImpl {
+import pe.com.peliculas.datos.*;
+import pe.com.peliculas.domain.Pelicula;
+import pe.com.peliculas.exceptions.*;
+
+public class CatalogoPeliculasImpl implements ICatalogoPeliculas {
+    private final IAccesoDatos datos;
+
+    public CatalogoPeliculasImpl() {
+        this.datos = new AccesoDatosImpl();
+    }
+
+    @Override
+    public void agregarPelicula(String nombrePelicula, String nombreArchivo) {
+        try {
+            datos.escribir(new Pelicula(nombrePelicula), nombreArchivo, true);
+        } catch (EscrituraDatosEx e) {
+            System.out.println("Error al agregar pelicula " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void listarPeliculas(String nombreArchivo) {
+        try {
+            datos.listar(nombreArchivo).forEach(pelicula -> System.out.println("La pelicula " + pelicula));
+        } catch (LecturaDatosEx e) {
+            System.out.println("Error al listar peliculas " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void buscarPelicula(String nombreArchivo, String dato) {
+        try {
+            System.out.println(datos.buscar(nombreArchivo, dato));
+        } catch (LecturaDatosEx e) {
+            System.out.println("Error al buscar pelicula" + e.getMessage());
+        }
+    }
+
+    @Override
+    public void iniciarArchivo(String nombreArchivo) {
+        try {
+            datos.crear(nombreArchivo);
+        } catch (AccesoDatosEx e) {
+            System.out.println("Error al iniciar archivo " + e.getMessage());
+        }
+    }
 }
--- 09-ProyectoPeliculas/pe/com/peliculas/negocio/ICatalogoPeliculas.java
+++ 09-ProyectoPeliculas/pe/com/peliculas/negocio/ICatalogoPeliculas.java
@@ -1,4 +1,11 @@
 package pe.com.peliculas.negocio;
 
 public interface ICatalogoPeliculas {
+    void agregarPelicula(String nombrePelicula, String nombreArchivo);
+
+    void listarPeliculas(String nombreArchivo);
+
+    void buscarPelicula(String nombreArchivo, String dato);
+
+    void iniciarArchivo(String nombreArchivo);
 }
