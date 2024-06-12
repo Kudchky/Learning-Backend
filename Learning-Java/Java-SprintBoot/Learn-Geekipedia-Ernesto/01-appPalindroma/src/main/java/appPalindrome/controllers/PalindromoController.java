d9ed4da38878d4387d33b8e8158765e1d40515e0
Pol Castillo <rpcastilloy@gmail.com>
2024-06-11 21:43:24 -0500

    Haciendo documentacion con javadoc

--- Learning-Java/Java-SprintBoot/Learn-Geekipedia-Ernesto/01-appPalindroma/src/main/java/appPalindrome/controllers/PalindromoController.java
+++ Learning-Java/Java-SprintBoot/Learn-Geekipedia-Ernesto/01-appPalindroma/src/main/java/appPalindrome/controllers/PalindromoController.java
@@ -2,16 +2,29 @@ package appPalindrome.controllers;
 
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Controlador para verificar palindromos
+ */
+
 @RestController
 public class PalindromoController {
 
-    //this is an endpoint
+    /**
+     * Endpoint para verificar si una palabra es un palindromo.
+     * @param word La palabra a verificar.
+     * @return Un mensaje si la palabra es un palindromo o no.
+     */
     @GetMapping("/validar-palindromo/{word}")
     public String validarPalindromo(@PathVariable String word) {
         return isPalindrome(word) ? "La palabra " + word + " SI es Palindromo" :
                 "La palabra " + word + " NO es Palindromo";
     }
 
+    /**
+     * Metodo para verificar si una palabra es un palindromo.
+     * @param word La palabra a verificar.
+     * @return true si la palabra es  palindromo, false de lo contario.
+     */
     private boolean isPalindrome(String word) {
         word = word.toLowerCase();
 
